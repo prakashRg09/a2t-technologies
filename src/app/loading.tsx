ccc4aea506f98baa0a27c087f4364974f925a84c
prakashRg09 <prakashRg09@gmail.com>
2025-01-06 12:11:49 +0530

    loader updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,10 @@
-import styles from './loading.module.scss'
-import './loading.module.scss'
+import styles from "./loading.module.scss";
+import "./loading.module.scss";
 
 export default function Loading() {
-     return (
-          <div className={styles.loadingContainer}>
-               <h1>Loading..,</h1>
-          </div>
-     )
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.loader}></div>
+    </div>
+  );
 }
