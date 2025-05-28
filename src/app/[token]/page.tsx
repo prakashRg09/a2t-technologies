065944b2efb8cf795a4fe4fa34d915bd4ab8e6ed
prakashRg09 <prakashRg09@gmail.com>
2025-05-28 15:25:50 +0530

    fixed issue

--- src/app/[token]/page.tsx
+++ src/app/[token]/page.tsx
@@ -1,9 +1,15 @@
+// src/app/[token]/page.tsx
 import React from 'react'
 import TokenPage from './TokenPage'
 
+// This tells Next.js that this dynamic route should be handled client-side
 export async function generateStaticParams() {
+     // Return empty array - all tokens will be handled client-side
      return []
 }
+
+// This tells Next.js to generate this route at build time
+export const dynamicParams = true
 
 function page() {
      return <TokenPage />
